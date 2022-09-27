af6177deaef0547cda47f4fad3ba4d2151029ec1
Arvind <ranaarvind156@gmail.com>
2022-09-27 18:40:37 +0530

    Contains Duplicate III

--- 050. Contains Duplicate III/Duplicates.py
+++ 050. Contains Duplicate III/Duplicates.py
@@ -1,11 +1,12 @@
 from sortedcontainers import SortedList
 def containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff):
     list = SortedList()
-
-    for i,num in enumerate(nums):
-            
+        
+    i,j = 0,0
+        
+    while j < len(nums):
         # To get the correct index to put the current element
-        idealIndex = SortedList.bisect_left(list, num)
+        idealIndex = SortedList.bisect_left(list, nums[j])
             
         # From this index, we can retrieve the floor and ceil
         # Floor will simply be the ideal index of current element - 1
@@ -15,17 +16,24 @@ def containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff):
         # Check if their difference is <= valueDiff
         # If yes, then return True as we found a pair
         if floor >= 0 and floor < len(list):
-            if num - list[floor] <= valueDiff: return True
+            if nums[j] - list[floor] <= valueDiff: return True
             
         # If there is a smallest value that is just larger than current number
         # Check if their difference is <= valueDiff
         # If yes, then return True as we found a pair
         if ceil >= 0 and ceil < len(list):
-            if list[ceil] - num <= valueDiff: return True
-
-        list.add(num)
-
-        if len(list) > indexDiff: list.remove(nums[i - indexDiff])
+            if list[ceil] - nums[j] <= valueDiff: return True
+                
+        # Add the current element in the list
+        list.add(nums[j])
+            
+        # If window size is not yet indexDiff + 1
+        if j - i + 1 < indexDiff + 1: j += 1
+        # If window size is reached, remove the first element of current window before moving to the next window
+        else:
+            list.remove(nums[i])
+            i += 1
+            j += 1
     
     return False
 
